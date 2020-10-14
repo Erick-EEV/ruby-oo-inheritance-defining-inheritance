19eff7b84b440957014ec014a0b70e777f044316
Erick-EEV <EEVERICKEEV@gmail.com>
2020-10-14 08:25:59 -0400

    Really Done

--- lib/car.rb
+++ lib/car.rb
@@ -1,25 +1,25 @@
 require_relative "./vehicle.rb"
 require 'pry'
-class Car
-    attr_accessor :wheel_size
-    def initialize(wheel_size, number)
-        @wheel_size = "small"
-        @number = 4
-    end
+class Car < Vehicle
+    # attr_accessor :wheel_size
+    # def initialize(wheel_size, number)
+    #     @wheel_size = "small"
+    #     @number = 4
+    # end
 
-    def wheel_number=(wheel_number)
-        @wheel_number = wheel_number
-    end
+    # def wheel_number=(wheel_number)
+    #     @wheel_number = wheel_number
+    # end
 
-    def wheel_number
-        @wheel_number
-    end
+    # def wheel_number
+    #     @wheel_number
+    # end
 
     def go
         return 'VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!'
     end
 
-    def fill_up_tank
-        return 'filling up!'
-    end
+    # def fill_up_tank
+    #     return 'filling up!'
+    # end
 end
